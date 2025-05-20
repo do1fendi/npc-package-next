ccc5c5a0d8da010475d3f70c79e3986154dcc5f4
Fendi Rusly <do1fendi@gmail.com>
2025-05-20 15:02:43 +0800

    test-app

---
+++ test-app/app/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import Image from "next/image";
+import { useInViewport } from "@do1fendi/next-lib";
+
+export default function Home() {
+  const [ref, isIntersecting] = useInViewport({ threshold: 0.25, rootMargin: "90% 0px -10% 0px" });
+  return (
+    <div className="">
+      <div className="h-screen">sdad</div>
+      <div ref={ref}>{JSON.stringify(isIntersecting)}</div>
+      <div className="h-screen">sdad</div>
+    </div>
+  );
+}
